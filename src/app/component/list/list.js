6e889dc82bd3e9eae39befc70cf5cbc78c2a6a86
chjeong23134 <chjeong23134@naver.com>
2019-12-02 19:03:53 +0900

    게시판 메뉴 추가

--- src/app/component/list/list.js
+++ src/app/component/list/list.js
@@ -1,10 +1,61 @@
+import moment from 'moment';
+
 class ListController {
     constructor() {
-        
+        this.list = [
+                        {
+                            name: '게시글 1',
+                            writer: 'chjeong',
+                            writeDate: moment('2019-12-01').format('YYYY-MM-DD'),
+                            writeTime: moment('183500', "hmmss").format('HH:mm:SS')
+                        },
+                        {
+                            name: '게시글 2',
+                            writer: 'chjeong',
+                            writeDate: moment('2019-12-01').format('YYYY-MM-DD'),
+                            writeTime: moment('183500', "hmmss").format('HH:mm:SS')
+                        },
+                        {
+                            name: '게시글 3',
+                            writer: 'chjeong',
+                            writeDate: moment('2019-12-01').format('YYYY-MM-DD'),
+                            writeTime: moment('183500', "hmmss").format('HH:mm:SS')
+                        },
+                        {
+                            name: '게시글 4',
+                            writer: 'chjeong',
+                            writeDate: moment('2019-12-01').format('YYYY-MM-DD'),
+                            writeTime: moment('183500', "hmmss").format('HH:mm:SS')
+                        },
+                        {
+                            name: '게시글 5',
+                            writer: 'chjeong',
+                            writeDate: moment('2019-12-01').format('YYYY-MM-DD'),
+                            writeTime: moment('183500', "hmmss").format('HH:mm:SS')
+                        },
+                        {
+                            name: '게시글 6',
+                            writer: 'chjeong',
+                            writeDate: moment('2019-12-01').format('YYYY-MM-DD'),
+                            writeTime: moment('183500', "hmmss").format('HH:mm:SS')
+                        },
+                        {
+                            name: '게시글 7',
+                            writer: 'chjeong',
+                            writeDate: moment('2019-12-01').format('YYYY-MM-DD'),
+                            writeTime: moment('183500', "hmmss").format('HH:mm:SS')
+                        }
+                    ];
+        this.nowDate = moment().format('YYYY[-]MM[-]DD');
+        this.nowTime = moment().format('HH[:]mm[:]SS')
     }
 
     $onInit() {
 
+    }
+
+    _getList() {
+        return this.list;
     }
 }
 
